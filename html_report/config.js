44dfff856d8edee6dc284b23186af363907cfc0c
MateoAgudGarcia <MateoAgudGarcia@users.noreply.github.com>
2025-04-15 23:30:18 +0000

    deploy: 6091e53c71d1e2d85b76405d653a5bb93eb9cc65

--- html_report/config.js
+++ html_report/config.js
@@ -1,10 +1,10 @@
-{
+report({
   "testSuite": "BackstopJS",
   "tests": [
     {
       "pair": {
         "reference": "../bitmaps_reference/Armenia_-_Cartelera_viewport_desktop.png",
-        "test": "../bitmaps_test/20250415-232821/Armenia_-_Cartelera_viewport_desktop.png",
+        "test": "../bitmaps_test/20250415-233012/Armenia_-_Cartelera_viewport_desktop.png",
         "selector": "viewport",
         "fileName": "Armenia_-_Cartelera_viewport_desktop.png",
         "label": "Armenia - Cartelera",
@@ -22,16 +22,16 @@
           },
           "rawMisMatchPercentage": 13.103636363636365,
           "misMatchPercentage": "13.10",
-          "analysisTime": 74
+          "analysisTime": 77
         },
-        "diffImage": "../bitmaps_test/20250415-232821/failed_diff_Armenia_-_Cartelera_viewport_desktop.png"
+        "diffImage": "../bitmaps_test/20250415-233012/failed_diff_Armenia_-_Cartelera_viewport_desktop.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Contactanos_PQRS_document_desktop.png",
-        "test": "../bitmaps_test/20250415-232821/Contactanos_PQRS_document_desktop.png",
+        "test": "../bitmaps_test/20250415-233012/Contactanos_PQRS_document_desktop.png",
         "selector": "document",
         "fileName": "Contactanos_PQRS_document_desktop.png",
         "label": "Contactanos PQRS",
@@ -49,16 +49,16 @@
           },
           "rawMisMatchPercentage": 4.006945096198968,
           "misMatchPercentage": "4.01",
-          "analysisTime": 98
+          "analysisTime": 96
         },
-        "diffImage": "../bitmaps_test/20250415-232821/failed_diff_Contactanos_PQRS_document_desktop.png"
+        "diffImage": "../bitmaps_test/20250415-233012/failed_diff_Contactanos_PQRS_document_desktop.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Medellin_-_Alternativo_divlanding-page__blockfirst-of-type_desktop.png",
-        "test": "../bitmaps_test/20250415-232821/Medellin_-_Alternativo_divlanding-page__blockfirst-of-type_desktop.png",
+        "test": "../bitmaps_test/20250415-233012/Medellin_-_Alternativo_divlanding-page__blockfirst-of-type_desktop.png",
         "selector": "div.landing-page__block:first-of-type",
         "fileName": "Medellin_-_Alternativo_divlanding-page__blockfirst-of-type_desktop.png",
         "label": "Medellin - Alternativo",
@@ -74,11 +74,13 @@
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 37
         }
       },
       "status": "pass"
     }
   ],
   "id": "Cine Colombia"
-}
+});
